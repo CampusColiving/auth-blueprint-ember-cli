cdb20e789329968fec8bbd471733ce78d90226c4
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2015-06-18 18:45:54 +0200

    adds google maps integration

--- config/environment.js
+++ config/environment.js
@@ -22,6 +22,7 @@ module.exports = function(environment) {
   ENV['simple-auth'] = {
     authorizer:               'simple-auth-authorizer:oauth2-bearer',
     routeAfterAuthentication: 'internal',
+    session:                  'session:custom',
   };
 
   ENV['simple-auth-oauth2'] = {
@@ -36,14 +37,18 @@ module.exports = function(environment) {
     }
   };
 
+  ENV.contentSecurityPolicy = {
+    'script-src':  "'self' maps.googleapis.com maps.gstatic.com 'unsafe-eval'",
+    'style-src':   "'self' 'unsafe-inline'",
+    'img-src':     "csi.gstatic.com maps.gstatic.com"
+  }
+
   if (environment === 'development') {
-    ENV.restAPINamespace      = '';
-    ENV.apiServer             = 'http://localhost:4200';
-    ENV.contentSecurityPolicy = {
-      'connect-src': "'self' http://localhost:4200",
-    }
     ENV.apiServer   = 'http://localhost:4200';
     ENV.zuoraServer = 'https://apisandbox-api.zuora.com';
+    ENV['simple-auth-oauth2'].serverTokenEndpoint = 'http://localhost:4200/token';
+
+    ENV.contentSecurityPolicy['connect-src'] = "'self' http://localhost:4200";
     // ENV.APP.LOG_RESOLVER = true;
     // ENV.APP.LOG_ACTIVE_GENERATION = true;
     // ENV.APP.LOG_TRANSITIONS = true;
@@ -62,16 +67,24 @@ module.exports = function(environment) {
 
     ENV.APP.rootElement = '#ember-testing';
 
-    ENV['simple-auth-oauth2'].serverTokenEndpoint = '/token';
-
     ENV.apiServer   = '';
     ENV.zuoraServer = '/zuora';
+
+    ENV['simple-auth'].store = 'simple-auth-session-store:ephemeral';
+
+    ENV['simple-auth-oauth2'].serverTokenEndpoint = '/token';
   }
 
   if (environment === 'production') {
     ENV.apiServer   = 'https://your.api.com';
     ENV.zuoraServer = 'https://api.zuora.com';
+    ENV.contentSecurityPolicy = {
+      'connect-src': "'self' https://your.api.com",
+    }
+    ENV['simple-auth-oauth2'].serverTokenEndpoint = 'https://your.api.com/token';
   }
+
+  ENV['simple-auth'].crossOriginWhitelist = [ENV.apiServer];
 
   return ENV;
 };
